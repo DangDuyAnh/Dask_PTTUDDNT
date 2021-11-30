d11f4f84fc8d3cf8ee859841e7b91939403ba047
Nguyen Huu Kiet <kietnguyen10112000@gmail.com>
2021-11-30 23:44:00 +0700

    + Add contact tab

--- client/screens/authentication/MainTab.js
+++ client/screens/authentication/MainTab.js
@@ -1,95 +1,141 @@
 import * as React from 'react';
 import { Ionicons, MaterialCommunityIcons, MaterialIcons} from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StyleSheet, View, Text, StatusBar, Touchable, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, StatusBar, Touchable, TouchableOpacity, TextInput } from 'react-native';
 
 import ChatTab from './chattab/ChatTab';
 import DiaryTab from './diarytab/DiaryTab';
 import ProfileTab from './profiletab/ProfileTab';
-import Contact from './contacttab/Contact';
+import { Contact } from './contacttab/Contact';
 import * as Const from '../../config/Constants';
 import * as RootNavigation from '../../RootNavigation';
 
 const Tab = createBottomTabNavigator();
 
-export default function MainTab() {
-    return (
-      <>
-        <StatusBar backgroundColor={Const.COLOR_THEME} />
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-  
-              if (route.name === 'ChatTab') {
-                iconName = focused ? 'chatbubble-ellipses' : 'chatbubble-ellipses-outline';
-                return <Ionicons name={iconName} size={size} color={color} />;
-              } else if (route.name === 'DiaryTab') {              
-                iconName = focused ? 'clock-time-eight' : 'clock-time-eight-outline';
-                return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
-              } else if (route.name === 'ProfileTab') {
-                iconName = focused ? 'person' : 'person-outline';
-                return <Ionicons name={iconName} size={size} color={color} />;
-              } else if (route.name === 'Contact') {
-                iconName = focused ? 'contacts' : 'contacts-outline';
-                return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
-              }
-            },
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: '100%',
+  },
+  headerWrapper: {
+    flexDirection: 'row', 
+    alignItems: "center",
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  headerText: {
+    fontSize: 18,
+    color: 'white'
+  },
+  input: {
+    height: 100,
+    width: '75%',
+    fontSize: 20,
+    color: 'white'
 
-            tabBarActiveTintColor: Const.COLOR_THEME,
-            tabBarInactiveTintColor: 'gray',
-          })}
-        >
-          <Tab.Screen name="ChatTab" component={ChatTab} />
-          <Tab.Screen name="Contact" component={Contact} />
-          <Tab.Screen name="DiaryTab" component={DiaryTab} 
-          options={{ 
-            headerTitle: header, 
-            headerStyle: {
-            backgroundColor: Const.COLOR_THEME,
-            },  
-            }}/>
-          <Tab.Screen name="ProfileTab" component={ProfileTab}/>
-        </Tab.Navigator>
-      </>
-    );
   }
+});
 
-  const header = () => {
-    return(
-      <View style={styles.headerContainer}>
-        <View style={styles.headerWrapper}>
-          <Ionicons style={{...styles.headerChild, paddingRight: 10}} name="search-outline" size={26} color="white" />
-          <Text style = {styles.headerText}>Tìm bạn bè, tin nhắn ...</Text>
-        </View>
-        <View style={styles.headerWrapper}>
-          <TouchableOpacity style={{paddingRight: 10}} onPress = {() => {
-            console.log('Hi');
-            RootNavigation.navigate('Post')}}>
-            <MaterialIcons name="post-add" size={26} color="white"  />
-          </TouchableOpacity>
-          <Ionicons name="ios-notifications-outline" size={26} color="white"  />
-        </View>
+function Header(props) {
+  const change = props.onChangeText;
+  const [text, onChangeText] = React.useState(props.searchText);
+  return(
+    <View style={styles.headerContainer}>
+      <View style={styles.headerWrapper}>
+        <Ionicons style={{...styles.headerChild, paddingRight: 10}} name="search-outline" size={26} color="white" />
+        <TextInput
+          style={styles.input}
+          onChangeText={(e)=>{onChangeText(e); }}
+          value={text}
+          placeholder="Tìm bạn bè, ..."
+          placeholderTextColor='white'
+          clearTextOnFocus='true'
+          selectionColor={'white'}
+          underlineColorAndroid={'transparent'}
+          onEndEditing={(e)=>{change(text);}}
+        />
       </View>
-    )
-  }
+      <View style={styles.headerWrapper}>
+        <TouchableOpacity style={{paddingRight: 10}} onPress = {() => {
+          console.log('Hi');
+          RootNavigation.navigate('Post')}}>
+          <MaterialIcons name="post-add" size={26} color="white"  />
+        </TouchableOpacity>
+        <Ionicons name="ios-notifications-outline" size={26} color="white"  />
+      </View>
+    </View>
+  )
+}
 
-  const styles = StyleSheet.create({
-    headerContainer: {
-      flexDirection: 'row',
-      alignItems: "center",
-      justifyContent: "space-between",
-      width: '100%',
-    },
-    headerWrapper: {
-      flexDirection: 'row', 
-      alignItems: "center",
-      paddingTop: 10,
-      paddingBottom: 10,
-    },
-    headerText: {
-      fontSize: 18,
-      color: 'white'
-    }
-  });
+export default function MainTab() {
 
+  const [searchText, setChangeSearchText] = React.useState("");
+
+
+  const header = <Header onChangeText={setChangeSearchText} searchText={searchText}/>;
+
+  return (
+    <>
+      <StatusBar backgroundColor={Const.COLOR_THEME} />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'ChatTab') {
+              iconName = focused ? 'chatbubble-ellipses' : 'chatbubble-ellipses-outline';
+              return <Ionicons name={iconName} size={size} color={color} />;
+            } else if (route.name === 'DiaryTab') {              
+              iconName = focused ? 'clock-time-eight' : 'clock-time-eight-outline';
+              return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+            } else if (route.name === 'ProfileTab') {
+              iconName = focused ? 'person' : 'person-outline';
+              return <Ionicons name={iconName} size={size} color={color} />;
+            } else if (route.name === 'Contact') {
+              iconName = focused ? 'contacts' : 'contacts-outline';
+              return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+            }
+          },
+
+          tabBarActiveTintColor: Const.COLOR_THEME,
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="ChatTab" component={ChatTab} 
+          options={{ 
+            headerTitle: ()=>{return (header)}, 
+            headerStyle: {
+              backgroundColor: Const.COLOR_THEME,
+            }
+          }}
+        />
+        <Tab.Screen name="Contact" children={ ()=>{return(<Contact input={searchText}/>)}} 
+          options={{ 
+            headerTitle: ()=>{return (header)}, 
+            headerStyle: {
+              backgroundColor: Const.COLOR_THEME,
+            }
+          }}
+        />
+        <Tab.Screen name="DiaryTab" component={DiaryTab} 
+          options={{ 
+            headerTitle: ()=>{return (header)}, 
+            headerStyle: {
+              backgroundColor: Const.COLOR_THEME,
+            }
+          }}
+          />
+        <Tab.Screen name="ProfileTab" component={ProfileTab}
+          options={{ 
+            headerTitle: ()=>{return (header)}, 
+            headerStyle: {
+              backgroundColor: Const.COLOR_THEME,
+            }
+          }}
+        />
+      </Tab.Navigator>
+    </>
+  );
+}
--- client/screens/authentication/contacttab/Contact.js
+++ client/screens/authentication/contacttab/Contact.js
@@ -1,8 +1,36 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 
-export default function Contact(props) {
-    return(
-        <Text> Hi </Text>
-    )
+import { Ionicons, MaterialCommunityIcons, MaterialIcons} from '@expo/vector-icons';
+
+import { StyleSheet, View, Text, StatusBar, Touchable, TouchableOpacity, TextInput } from 'react-native';
+
+import { authPost } from '../../../api/api'
+
+import { LogBox } from 'react-native';
+
+//LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
+LogBox.ignoreAllLogs();//Ignore all log notifications
+
+//console.warn = () => {};
+
+function Contact(props) {
+
+  const searchText = props.input;
+
+  const [text, onChangeText] = React.useState("");
+
+  let body = {
+    keyword : '0986609276'
+  }
+  let res = authPost('/users/search', body);
+  res.then((data)=>{
+    onChangeText(JSON.stringify(data));
+  })
+
+  //https_url/search
+  return(
+    <Text> {text} </Text>
+  )
 }
+
+export { Contact };
